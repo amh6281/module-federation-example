7786546c8b2b54a4ed77dac2d0e3b848c6ae1fcc
amh6281 <amh6281@naver.com>
2025-05-23 08:13:59 +0900

    초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { lazy } from "react";
+
+import "@mailplug-inc/design-system/dist/assets/style.css";
+
+const TwoButton = lazy(() => import("task/TwoButton"));
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Host App</h1>
+      <TwoButton />
+    </div>
+  );
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "host",
+      remotes: {
+        task: "http://mytask.wiro.kr:5000/dist/assets/taskRemoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+});
