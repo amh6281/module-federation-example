e1304918b2477511437e4b07f55ff5e141fbe359
amh6281 <amh6281@naver.com>
2025-05-26 08:42:38 +0900

    초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { useEffect } from "react";
-
 const App = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "http://localhost:4173/task-element.js";
-
-    script.type = "text/javascript";
-    document.body.appendChild(script);
-  }, []);
-
-  return (
-    <div>
-      host app
-      <app-task />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
