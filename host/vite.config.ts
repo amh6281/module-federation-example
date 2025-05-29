6d6c5d0485615b69de27a1dcc99337caaf6c0309
amh6281 <amh6281@naver.com>
2025-05-29 16:23:09 +0900

    제거

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
-import pkg from "./package.json";
-import { resolve } from "path";
 
-const { dependencies } = pkg;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,26 +11,7 @@ export default defineConfig({
       remotes: {
         zustand: "http://localhost:5173/dist/assets/zustandRemoteEntry.js",
       },
-      shared: {
-        react: {
-          version: dependencies["react"],
-          requiredVersion: dependencies["react"],
-          packagePath: resolve(__dirname, "node_modules/react"),
-        },
-        "react-dom": {
-          version: dependencies["react-dom"],
-          requiredVersion: dependencies["react-dom"],
-          packagePath: resolve(__dirname, "node_modules/react-dom"),
-        },
-        zustand: {
-          version: dependencies["zustand"],
-          requiredVersion: dependencies["zustand"],
-        },
-        "use-sync-external-store": {
-          version: dependencies["use-sync-external-store"],
-          requiredVersion: dependencies["use-sync-external-store"],
-        },
-      },
+      shared: ["react", "react-dom", "zustand", "use-sync-external-store"],
     }),
   ],
   build: {
