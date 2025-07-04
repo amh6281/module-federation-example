fb63c15f4d75e63a182db3902b40fe5e38795ed0
amh6281 <amh6281@naver.com>
2025-07-04 10:31:56 +0900

    scss test

--- remote/src/App.jsx
+++ remote/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./global.css";
+import "./hello.scss";
 
 const App = () => {
   return (
     <div>
       <h1>Remaaote Aaaapdddp</h1>
+      <h2 className="hello">Hello World</h2>
     </div>
   );
 };
