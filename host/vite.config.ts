cc9addd0a6c21e62ad3b10b037d556b34305fedd
amh6281 <amh6281@naver.com>
2025-07-17 14:37:05 +0900

    navite-federation-typescript builderror

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import { NativeFederationTypeScriptHost } from "@module-federation/native-federation-typescript";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,15 @@ export default defineConfig({
       remotes: {
         remote: "http://localhost:4001/assets/remoteEntry.js",
       },
+    }),
+    NativeFederationTypeScriptHost.vite({
+      moduleFederationConfig: {
+        remotes: {
+          remote: "/remote/types/remote.d.ts",
+        },
+      },
+      typesFolder: "./types", // 타입 저장 위치
+      deleteTypesFolder: false, // 필요 시 true
     }),
   ],
 });
--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import { NativeFederationTypeScriptRemote } from "@module-federation/native-federation-typescript";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,17 @@ export default defineConfig({
       exposes: {
         "./Button": "./src/components/Button.tsx",
       },
+    }),
+    NativeFederationTypeScriptRemote.vite({
+      moduleFederationConfig: {
+        exposes: {
+          "./Button": "./src/components/Button.tsx",
+        },
+      },
+      tsConfigPath: "./tsconfig.json",
+      typesFolder: "./dist/types/remote",
+      compiledTypesFolder: "./dist/compiled-types",
+      deleteTypesFolder: true, // 배포 전 타입 폴더 정리 원할 경우
     }),
   ],
 });
