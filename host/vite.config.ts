ebc5bc211c8fbee911cdd143b7d2fe1e50bfc4a5
amh6281 <amh6281@naver.com>
2025-07-22 12:57:05 +0900

    reset

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,30 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
-import { NativeFederationTypeScriptHost } from "@module-federation/native-federation-typescript/vite";
-import path from "path";
 
-const moduleFederationConfig = {
-  name: "host",
-  filename: "hostEntry.js",
-  remotes: {
-    remote: "http://localhost:4001/assets/remoteEntry.js",
-  },
-  shared: ["react", "react-dom"],
-};
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    federation(moduleFederationConfig),
-    NativeFederationTypeScriptHost({
-      moduleFederationConfig,
-      typesFolder: path.resolve(__dirname, "../@mf-types"), // 공통 타입 저장 위치
-      deleteTypesFolder: true,
-      maxRetries: 3,
+    federation({
+      name: "host",
+      filename: "hostEntry.js",
+      remotes: {
+        remote: "http://localhost:4001/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
     }),
   ],
-  server: {
-    port: 3000,
-  },
 });
--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -1,37 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
-import { NativeFederationTypeScriptRemote } from "@module-federation/native-federation-typescript/vite";
-import path from "path";
 
-const moduleFederationConfig = {
-  name: "remote", // 실제 사용할 remote 이름
-  filename: "remoteEntry.js",
-  exposes: {
-    "./Button": "./src/components/button", // 상대경로 확실히 맞춰주세요
-  },
-  shared: ["react", "react-dom"],
-};
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    federation(moduleFederationConfig),
-    NativeFederationTypeScriptRemote({
-      moduleFederationConfig,
-      tsConfigPath: path.resolve(__dirname, "tsconfig.json"),
-      typesFolder: path.resolve(__dirname, "../@mf-types"), // 타입은 루트 외부에 모아서 저장
-      compiledTypesFolder: path.resolve(__dirname, "public/compiled-types"), // 빌드된 d.ts 위치
-      deleteTypesFolder: true,
+    federation({
+      name: "remote",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/components/Button.tsx",
+      },
+      shared: ["react", "react-dom"],
     }),
   ],
-  server: {
-    port: 3001,
-    fs: {
-      allow: ["."],
-    },
-  },
-  build: {
-    outDir: "dist",
-  },
 });
