6cefca1da92a61e01520aa5bd231ee2445933e95
amh6281 <amh6281@naver.com>
2025-09-09 09:40:59 +0900

    sync-types 관련 로직 제거

--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { federation } from "@module-federation/vite";
 import dts from "vite-plugin-dts";
-import { exec } from "child_process";
-import { promisify } from "util";
-
-const execAsync = promisify(exec);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -22,28 +18,7 @@ export default defineConfig({
     react(),
     dts({
       outDir: "dist/mf-types",
-      include: ["src/App.tsx"],
-      insertTypesEntry: true,
-      staticImport: true,
-      rollupTypes: false,
-      copyDtsFiles: false,
       tsconfigPath: "./tsconfig.app.json",
-      compilerOptions: {
-        declaration: true,
-        emitDeclarationOnly: true,
-        skipLibCheck: true,
-      },
-      afterBuild: async () => {
-        const typesDir = `dist/mf-types`;
-        const tarPath = `dist/mf-types.tar.gz`;
-
-        // tar 압축 후 원본 폴더 삭제
-        await execAsync(
-          `tar -czf "${tarPath}" -C "${typesDir}" . && rm -rf "${typesDir}"`
-        );
-      },
-
-      exclude: ["src/DnD.tsx"],
     }),
     federation({
       name: "remote",
