edf3325caa91e8b596df37efba98c2097dc97971
amh6281 <amh6281@naver.com>
2025-10-01 09:40:59 +0900

    tailwind v3 test

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,14 +1,15 @@
 import { lazy, Suspense } from "react";
-
-const RemoteApp = lazy(() => import("remote/RemoteApp"));
+import "./index.css";
+// const RemoteApp = lazy(() => import("remote/RemoteApp"));
 
 const App = () => {
   return (
     <div>
-      <h1>Host Application</h1>
-      <Suspense fallback={<div>Loading Remote App...</div>}>
+      <h1 className="bg-red-500">Host Application</h1>
+
+      {/* <Suspense fallback={<div>Loading Remote App...</div>}>
         <RemoteApp />
-      </Suspense>
+      </Suspense> */}
     </div>
   );
 };
