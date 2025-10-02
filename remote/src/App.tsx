7852ae2f7c170305c04f073e16eb97d51aa61e9e
amh6281 <amh6281@naver.com>
2025-10-02 11:39:45 +0900

    border hover test

--- remote/src/App.tsx
+++ remote/src/App.tsx
@@ -7,9 +7,11 @@ const App = () => {
 
   return (
     <div className="border-2 border-blue-500 p-4 m-2 rounded-lg bg-blue-50">
+      <h3 className="border hover:text-red-500 hover:border-amber-500 border-red-500">
+        Hover Text
+      </h3>
       <h1>helo</h1>
       <h2>🚀 Remote Application</h2>
-
       {/* Zustand Counter */}
       <div className="mb-7.5">
         <h3>📊 Zustand Counter</h3>
