d91076cc4dbd0e9e557c603ec624de1af43f4458
amh6281 <amh6281@naver.com>
2025-10-28 08:43:58 +0900

    playwright webServer 설정

--- remote/playwright.config.ts
+++ remote/playwright.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   testDir: "./tests",
   timeout: 30 * 1000,
   retries: 1,
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:3001",
+    timeout: 120 * 1000,
+    reuseExistingServer: true,
+  },
   use: {
     headless: true, // GUI 표시 여부
     baseURL: "http://localhost:3001", // remote 앱 URL
