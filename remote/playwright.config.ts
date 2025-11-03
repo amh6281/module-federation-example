daf0eb5097321c718cd550645d572afa3fb4e98e
amh6281 <amh6281@naver.com>
2025-11-03 14:34:54 +0900

    playwright baseurl

--- remote/playwright.config.ts
+++ remote/playwright.config.ts
@@ -6,12 +6,11 @@ export default defineConfig({
   retries: 1,
   webServer: {
     command: "npm run start",
-    url: "http://localhost:3001",
     timeout: 120 * 1000,
     reuseExistingServer: true,
   },
   use: {
     headless: true, // GUI 표시 여부
-    baseURL: "http://localhost:3001", // remote 앱 URL
+    baseURL: "http://localhost:3001/remote/", // remote 앱 URL
   },
 });
