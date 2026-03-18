b19dcf0293e83ea35823cc625f130687fe5ae04e
amh6281 <amh6281@naver.com>
2026-03-18 11:10:52 +0900

    test

--- host/src/Title.tsx
+++ host/src/Title.tsx
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+
 const Title = ({ title }: { title: string }) => {
+  useEffect(() => {
+    console.log("Title", title);
+  });
   return <h1 className="text-2xl font-bold text-center">{title}</h1>;
 };
 
